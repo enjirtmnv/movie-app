6a2153020ef746cbbe6bb2d2987928275585b11c
enjirtmnv <shkensinio@gmail.com>
2022-10-22 01:46:05 +0300

    change app name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between p-4 z-[100] absolute w-full'>
-        <h1  className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
+        <h1  className='text-red-600 text-4xl font-bold cursor-pointer'>ZETFLIX</h1>
         <div>
             <button className='text-white px-4'>Sign In</button>
             <button className='text-white bg-red-600 px-6 py-2 rounded'>Sign Up</button>
